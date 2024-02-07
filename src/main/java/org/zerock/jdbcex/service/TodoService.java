8c22e9541ce52cb54909a03aee54c68d1d8db4ff
abc20210708 <gotqlc2670@naver.com>
2024-02-07 20:32:34 +0900

    [24.02.06(수)] p.142 Log4j2와 @Log4j2

--- src/main/java/org/zerock/jdbcex/service/TodoService.java
+++ src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -1,11 +1,13 @@
 package org.zerock.jdbcex.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.zerock.jdbcex.dao.TodoDAO;
 import org.zerock.jdbcex.domain.TodoVo;
 import org.zerock.jdbcex.dto.TodoDTO;
 import org.zerock.jdbcex.util.MapperUtil;
 
+@Log4j2
 public enum TodoService {
     INSTANCE;
 
@@ -22,7 +24,8 @@ public enum TodoService {
 
         TodoVo todoVo = modelMapper.map(todoDTO, TodoVo.class);
 
-        System.out.println("todoVO: " + todoVo);
+        //System.out.println("todoVO: " + todoVo);
+        log.info(todoVo);
 
         dao.insert(todoVo); //int를 변환하므로 이를 이용해서 예외처리도 가능
     }
